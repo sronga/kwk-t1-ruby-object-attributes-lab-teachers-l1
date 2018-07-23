477b53c82c57d0f54b236865f1e5e5e014721bde
sronga <sr899@georgetown.edu>
2018-07-23 23:58:56 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,12 +8,12 @@ class Person
     @name = name
   end
 
-  def breed
-    @breed
+  def job
+    @job
   end
 
-  def breed=(breed)
-    @breed = breed
+  def job=(job)
+    @job = job
   end
 
 end
